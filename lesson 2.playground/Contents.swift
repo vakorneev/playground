7f354aad9492de96488f7ee4b90f960f0d1a796a
KORNEEV Viktor <viktor.korneev@raiffeisen.ru>
2025-02-01 17:36:02 +0300

    jnnjnjdifjnifg

--- lesson 2.playground/Contents.swift
+++ lesson 2.playground/Contents.swift
@@ -7,13 +7,7 @@ print("Hello World")
 
 
 func ask(_ text: String) -> String {
-  if text == "1" {
-    return "hello"
-  } else {
-
-    /// ojsndojnfsdojnsdfnj
-    return "bye bye"
-  }
+  return "mkdsfkm"
 }
 
 ask("123")
